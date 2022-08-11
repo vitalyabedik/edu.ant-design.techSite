f1eb50790984cbc6fcbd1445b764108a8f4e49ac
Vitali Bedik <vitalya.bedik@gmail.com>
2022-08-11 20:13:25 +0300

    creating project and header and carousel components

---
+++ src/components/common/header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { Menu } from 'antd';
+
+function AppHeader() {
+    const menuItems = [
+        {
+            label: `Home`,
+            key: 'home',
+        },
+        {
+            label: `About`,
+            key: 'about',
+        },
+        {
+            label: `Features`,
+            key: 'features',
+        },
+        {
+            label: `How it works`,
+            key: 'howitworks',
+        },
+        {
+            label: `FAQ`,
+            key: 'faq',
+        },
+        {
+            label: `Pricing`,
+            key: 'pricing',
+        },
+        {
+            label: `Contact`,
+            key: 'contact',
+        },
+    ];
+    return (
+        <div className='container-fluid'>
+            <div className='header'>
+                <div className='logo'>
+                    <i class='fas fa-bolt'></i>
+                    <a href='http://www.google.com'>Tech</a>
+                </div>
+                <Menu
+                    mode='horizontal'
+                    defaultSelectedKeys={['home']}
+                    items={menuItems}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default AppHeader;
