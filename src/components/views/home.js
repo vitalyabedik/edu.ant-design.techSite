c27de63670fd8e74cdc00d53a973db28b2927eca
Vitali Bedik <vitalya.bedik@gmail.com>
2022-08-11 22:04:48 +0300

    creating about component

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import AppHero from '../home/hero';
+import AppAbout from '../home/about';
 
 function AppHome() {
     return (
         <div className='main'>
             <AppHero />
+            <AppAbout />
         </div>
     );
 }
