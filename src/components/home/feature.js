e85e055ae983a920846c419f86df215e0b1c4396
Vitali Bedik <vitalya.bedik@gmail.com>
2022-08-12 23:43:13 +0300

    add anchor component to header

--- src/components/home/feature.js
+++ src/components/home/feature.js
@@ -12,7 +12,7 @@ const { Meta } = Card;
 
 function AppFeature() {
     return (
-        <div className='block featureBlock bgGray'>
+        <div id='feature' className='block featureBlock bgGray'>
             <div className='container-fluid'>
                 <div className='titleHolder'>
                     <h2>Key Features and Benefits</h2>
